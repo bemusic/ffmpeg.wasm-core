d56feb90018be2e47fc9d0a503f4e1ea3f977cc6
Thai Pangsakulyanont <dtinth@spacet.me>
2021-10-09 19:26:28 +0000

    Remove some more libs

--- wasm/build-scripts/build-ffmpeg.sh
+++ wasm/build-scripts/build-ffmpeg.sh
@@ -22,7 +22,7 @@ FLAGS=(
   -I. -I./fftools -I$BUILD_DIR/include
   -Llibavcodec -Llibavdevice -Llibavfilter -Llibavformat -Llibavresample -Llibavutil -Llibass -Lfribidi -Llibswscale -Llibswresample -L$BUILD_DIR/lib
   -Wno-deprecated-declarations -Wno-pointer-sign -Wno-implicit-int-float-conversion -Wno-switch -Wno-parentheses -Qunused-arguments
-  -lavdevice -lavfilter -lavformat -lavcodec -lswresample -lswscale -lavutil -lpostproc -lm -lharfbuzz -lvorbis -lvorbisenc -lvorbisfile -logg -lz -lopus
+  -lavdevice -lavfilter -lavformat -lavcodec -lswresample -lswscale -lavutil -lm -lvorbis -lvorbisenc -lvorbisfile -logg -lz -lopus
   fftools/ffmpeg_opt.c fftools/ffmpeg_filter.c fftools/ffmpeg_hw.c fftools/cmdutils.c fftools/ffmpeg.c
   -s USE_SDL=2                                  # use SDL2
   -s INVOKE_RUN=0                               # not to run the main() in the beginning
